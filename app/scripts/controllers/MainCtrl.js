cbd7595e3bd211be46c7d08f1bc51b6dbb2c5b82
Harold Asai <koichiroasai@gmail.com>
2017-04-22 10:37:17 -0700

    bloc-chat-checkpoint3-completed

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,9 +1,20 @@
 (function() {
-    function MainCtrl(Room) {
+    function MainCtrl(Room, $uibModal) {
         this.chatRoomArray = Room.all;
+        this.animationsEnabled = true;
+
+        this.openNewRoomModal = function(){
+            var modalInstance = $uibModal.open({
+                animation: this.animationsEnabled,
+                templateUrl: '/templates/new_room_modal.html',
+                controller: 'ModalCtrl',
+                controllerAs: 'modal'
+            });
+        };
     }
 
     angular
         .module('blocChat')
-        .controller('MainCtrl', ['Room', MainCtrl]);
+        .controller('MainCtrl', ['Room', '$uibModal', MainCtrl]);
+
 })();
--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,11 +1,22 @@
 (function() {
   function Room($firebaseArray) {
+    var object = {};
+
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
 
-    return {
-      all: rooms
+    object.all = rooms;
+
+    object.addNewRoom = function(roomName) {
+        rooms.$add(roomName).then(function(ref) {
+          var id = ref.key;
+          console.log("added "+ roomName + " with id " + id);
+          rooms.$indexFor(id); // returns location in the array
+        });
     };
+
+
+    return object;
   }
 
   angular
