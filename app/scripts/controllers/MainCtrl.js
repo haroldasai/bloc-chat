df8309100fd9fa0a8f06948d7f538affffa686b7
Harold Asai <koichiroasai@gmail.com>
2017-04-24 17:12:45 -0700

    bloc-chat-checkpoint6-almostdone

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,33 +1,36 @@
 (function() {
-    function MainCtrl(Room, Message, $uibModal) {
+    function MainCtrl(Room, Message, $uibModal, $cookies) {
         this.chatRoomArray = Room.all;
         this.animationsEnabled = true;
         this.currentRoomTitle = "";
         this.currentRoomId = "";
+        this.currentUser = $cookies.get('blocChatCurrentUser');
 
         this.openNewRoomModal = function(){
-            var modalInstance = $uibModal.open({
-                animation: this.animationsEnabled,
-                templateUrl: '/templates/new_room_modal.html',
-                controller: 'RoomModalCtrl',
-                controllerAs: 'roommodal'
-            });
+          var modalInstance = $uibModal.open({
+              animation: this.animationsEnabled,
+              templateUrl: '/templates/new_room_modal.html',
+              controller: 'RoomModalCtrl',
+              controllerAs: 'roommodal'
+          });
         };
 
         this.setCurrentRoom = function(name, id) {
-            this.currentRoomTitle = name;
-            this.currentRoomId = id;
-            this.messageArray = Message.getByRoomId(id);
+          this.currentRoomTitle = name;
+          this.currentRoomId = id;
+          this.messageArray = Message.getByRoomId(id);
         };
 
-
-
-
+        this.sendMessage = function() {
+          var currentTime = "1:00pm";
+          Message.send(this.message, this.currentRoomId, currentTime, this.currentUser);
+          this.message = "";
+        }
 
     }
 
     angular
         .module('blocChat')
-        .controller('MainCtrl', ['Room', 'Message', '$uibModal', MainCtrl]);
+        .controller('MainCtrl', ['Room', 'Message', '$uibModal', '$cookies', MainCtrl]);
 
 })();
--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -8,6 +8,14 @@
         // Filter the messages by their room ID.
         var array = $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));
         return array;
+      },
+
+      send: function (newMessage, roomname, time, usrname) {
+        messages.$add({content: newMessage, roomId: roomname, sentAt: time, username: usrname}).then(function(ref) {
+          var id = ref.key;
+          console.log("added new message with id " + id);
+          messages.$indexFor(id); // returns location in the array
+        });
       }
     };
   }
